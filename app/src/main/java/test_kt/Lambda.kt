536495c27d61407d462a80c09e648af6566cd543
yzh <295658413@qq.com>
2020-09-14 08:59:00 +0800

    commit:zap:

--- app/src/main/java/test_kt/Lambda.kt
+++ app/src/main/java/test_kt/Lambda.kt
@@ -23,4 +23,36 @@ fun main() {
 //
 //    (T, (P,Q) -> S) -> R//表示接收一个T类型参数和一个接收P、Q类型两个参数并返回一个S类型的值的Lambda表达式类型参数，返回一个R类型值的Lambda表达式类型
 
+
+/*    完整表达方式：
+    val 函数名 : (参数1类型, 参数2类型, ...) -> 返回值类型 = { 参数1, 参数2, ... -> 函数体 }
+
+    表达式返回值类型可自动推断形式
+    val 函数名 = { 参数1:类型1, 参数2:类型2, ... -> 函数体 }*/
+
+    val sum1: (Int, Int) -> Int = { a, b -> a + b }
+    // 等价于
+    val sum2 = { a: Int, b: Int -> a + b }
+
+    // 等价于函数
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+
+
+/*    匿名函数
+    匿名函数形式为：
+
+    val 函数名 = fun(参数1:类型1, 参数2:类型2, ...): 返回值类型 { 函数体 }
+    示例：*/
+
+    val sum3 = fun(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    // 等价于函数
+    fun sum4(a: Int, b: Int): Int {
+        return a + b
+    }
 }
